5b9e4f69caadaae7671add09559104be496ac598
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-09 19:35:17 -0300

    feat: added validate password in serializers.py

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,5 +1,7 @@
-from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
 
 User = get_user_model()
 
@@ -8,6 +10,16 @@ class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("first_name", "last_name", "email", "password")
+
+    def validate(self, data):
+        user = User(**data)
+        password = data.get("password")
+
+        try:
+            validate_password(password=password, user=user)
+        except ValidationError as error:
+            serializers_errors = serializers.as_serializer_error(error)
+            raise ValidationError({"password": serializers_errors["non_field_errors"]}) from error
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
