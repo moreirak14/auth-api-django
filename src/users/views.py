fc6ad652d1241e7b4bd48d65dd1d32d9bb1d0171
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-09 19:14:46 -0300

    feat: create user endpoint post

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CreateUserSerializer, UserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = CreateUserSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user = serializer.create(serializer.validated_data)
+        user = UserSerializer(user)
+
+        return Response(data=user.data, status=status.HTTP_201_CREATED)
