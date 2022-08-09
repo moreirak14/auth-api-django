f1c6c64a87b80b914a5055f3990e709cd3340b8a
moreirak14 <kaiquemoreiracloud@gmail.com>
2022-08-09 20:24:38 -0300

    feat: create get_user endpoint

--- src/users/views.py
+++ src/users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -17,3 +17,13 @@ class RegisterView(APIView):
         user = UserSerializer(user)
 
         return Response(data=user.data, status=status.HTTP_201_CREATED)
+
+
+class RetrieveUserView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        user = UserSerializer(user)
+
+        return Response(data=user.data, status=status.HTTP_200_OK)
